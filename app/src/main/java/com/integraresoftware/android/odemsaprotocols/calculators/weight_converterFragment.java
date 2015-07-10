351ecd03ceb03aadf3837b740faccbb8eaba86db
chris <burrusscl@gmail.com>
2015-07-10 07:39:59 -0400

    nice update

--- app/src/main/java/com/integraresoftware/android/odemsaprotocols/calculators/weight_converterFragment.java
+++ app/src/main/java/com/integraresoftware/android/odemsaprotocols/calculators/weight_converterFragment.java
@@ -9,7 +9,8 @@ import android.view.ViewGroup;
 import com.integraresoftware.android.odemsaprotocols.R;
 
 /**
- * A placeholder fragment containing a simple view.
+ * This is the brains of the weight converter calculator
+ * It will automatically convert the weight after on focus lost
  */
 public class weight_converterFragment extends Fragment {
 
@@ -20,5 +21,12 @@ public class weight_converterFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_weight_converter, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
     }
 }
